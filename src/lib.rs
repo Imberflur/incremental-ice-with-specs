3cf9130150bd07df22e2392ad33dbb89949e00ed
Imbris <imbrisf@gmail.com>
2021-05-26 23:10:22 -0400

    Copied and reduced several traits from specs

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,92 @@
-use specs::{
-    shred::ResourceId, Component, Entities, ParJoin, ReadStorage, System, SystemData, World,
-};
+use specs::shred::Fetch;
+use specs::shred::MetaTable;
+use specs::storage::AnyStorage;
+use specs::storage::MaskedStorage;
+use specs::Component;
+use specs::Storage;
+use specs::World;
 
-pub struct Pos;
+pub type ReadStorage<'a, T> = Storage<'a, T, Fetch<'a, MaskedStorage<T>>>;
+
+trait System<'a> {
+    type SystemData;
+
+    fn run(data: Self::SystemData, world: &mut World);
+}
+
+struct Pos;
 
 impl Component for Pos {
     type Storage = specs::VecStorage<Self>;
 }
 
-pub struct InterpBuffer<T> {
+struct InterpBuffer<T> {
     _t: T,
 }
 
 impl<T: 'static + Send + Sync> Component for InterpBuffer<T> {
     type Storage = specs::VecStorage<Self>;
 }
-
-#[derive(SystemData)]
-pub struct ReadData<'a> {
+struct ReadData<'a> {
     pos_interpdata: ReadStorage<'a, InterpBuffer<Pos>>,
 }
 
-#[derive(Default)]
-pub struct Sys;
+struct Sys;
 
 impl<'a> System<'a> for Sys {
     type SystemData = (ReadData<'a>, ReadStorage<'a, Pos>);
 
-    fn run(&mut self, (data, pos): Self::SystemData) {
-        (&pos, &data.pos_interpdata).par_join();
+    fn run((data, pos): Self::SystemData, world: &mut World) {
+        <ReadStorage<'a, InterpBuffer<Pos>> as SystemData>::setup(world);
+
+        ParJoin::par_join((&pos, &data.pos_interpdata));
     }
 }
+
+trait ParJoin {
+    fn par_join(self)
+    where
+        Self: Sized,
+    {
+    }
+}
+
+impl<'a, 'e, T, D> ParJoin for &'a specs::Storage<'e, T, D>
+where
+    T: specs::Component,
+    D: core::ops::Deref<Target = specs::storage::MaskedStorage<T>>,
+    T::Storage: Sync,
+{
+}
+
+impl<A, B> ParJoin for (A, B)
+where
+    A: ParJoin,
+    B: ParJoin,
+{
+}
+
+//
+//pub trait Component: Any + Sized {
+//    type Storage: UnprotectedStorage<Self> + Any + Send + Sync;
+//}
+
+pub trait SystemData<'a> {
+    /// Sets up the system data for fetching it from the `World`.
+    fn setup(world: &mut World);
+}
+
+impl<'a, T> SystemData<'a> for ReadStorage<'a, T>
+where
+    T: Component,
+{
+    fn setup(res: &mut World) {
+        res.fetch_mut::<MetaTable<dyn AnyStorage>>()
+            .register(&*res.fetch::<MaskedStorage<T>>());
+    }
+}
+/*
+pub struct MaskedStorage<T: Component> {
+    mask: specs::hibitset::BitSet,
+    inner: T::Storage,
+}*/
