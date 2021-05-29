93829d0f72eb1296bf31fe37c486a9155e1ec4ec
Imbris <imbrisf@gmail.com>
2021-05-28 23:22:10 -0400

    Remove MetaTable

--- src/lib.rs
+++ src/lib.rs
@@ -88,10 +88,10 @@ impl<T: 'static> SystemData for ReadStorage<T>
 where
     T: Component,
 {
-    fn setup(mut any: Box<dyn Any>) {
-        let fetch: &mut (MetaTable<dyn Any>, MaskedStorage<T>) = any.downcast_mut().unwrap();
+    fn setup(any: Box<dyn Any>) {
+        let storage: &MaskedStorage<T> = any.downcast_ref().unwrap();
 
-        fetch.0.register(&fetch.1);
+        <dyn Any as CastFrom<MaskedStorage<T>>>::cast(&storage);
     }
 }
 
@@ -99,27 +99,8 @@ pub struct MaskedStorage<T: Component> {
     _inner: T::Storage,
 }
 
-struct Invariant<T: ?Sized>(*mut T);
-
-unsafe impl<T> Send for Invariant<T> where T: ?Sized {}
-
-unsafe impl<T> Sync for Invariant<T> where T: ?Sized {}
-
 pub unsafe trait CastFrom<T> {
     fn cast(t: &T) -> &Self;
-}
-
-pub struct MetaTable<T: ?Sized> {
-    marker: PhantomData<Invariant<T>>,
-}
-
-impl<T: ?Sized> MetaTable<T> {
-    pub fn register<R>(&mut self, r: &R)
-    where
-        T: CastFrom<R> + 'static,
-    {
-        <T as CastFrom<R>>::cast(r);
-    }
 }
 
 unsafe impl<T> CastFrom<T> for dyn Any
